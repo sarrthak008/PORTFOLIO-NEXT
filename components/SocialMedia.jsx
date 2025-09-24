3f7ed0fb7becfbd1ef34d27330365f1a429a530e
sarrthak008 <sarthaknavale08@gmail.com>
2025-09-24 14:38:27 +0530

    add a social media links

--- components/SocialMedia.jsx
+++ components/SocialMedia.jsx
@@ -8,18 +8,18 @@ const SocialMedia = () => {
   return (
     <div className='w-screen min-h-[50vh] px-4 relative overflow-hidden'>
 
-      <div className="w-[70%] mx-auto mt-5">
+      <div className="w-[90%] md:w-[70%] mx-auto mt-5">
         <h2 className='text-pink-600 text-2xl'>Connect With Me</h2>
         <p className='text-sm text-gray-400'>
           “Follow me on social media to stay updated on my latest projects, insights, and creative work”
         </p>
       </div>
 
-      <div className="flex w-[70%] flex-wrap">
+      <div className="flex w-[100%] md:w-[70%] flex-wrap mx-auto gap-5 mt-10">
          {
           SOCIAL_MEDIA_ACCOUNTS.map((media,index)=>(
-            <div className="bg-red-300" key={index}>
-              <span>{media.name}</span>
+            <div className=" border-gray-300  text-white border px-6 py-1 cursor-pointer" key={index} >
+              <a href={media.url} target="_blank" rel="noopener noreferrer"><span> <i className={media.logo}></i>{media.name}</span></a>
             </div>
           ))
          }
